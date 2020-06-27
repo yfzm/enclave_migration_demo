0d195219d63bcbfd4d75a0a54111a523aaa14935
Hao Li <yfzm1223@sjtu.edu.cn>
2020-06-28 01:25:46 +0800

    fix SYS_munmap bug && change machine

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,12 @@
 working_path=${1:-libquantum}
 (make ${working_path}) || exit 1
-scp ${working_path}/final_main_x86_64 wxy:~/workspace/sgx-driver/enclave/enclave
-scp ${working_path}/final_main_x86_64 wxy:~/workspace/sgx-driver/enclave/enclave_x86_64
-scp ${working_path}/final_main_aarch64 wxy:~/workspace/sgx-driver/enclave/enclave_aarch64
-scp ${working_path}/final_main_aarch64 tmac-arm:~/workspace/sgx-driver/enclave/enclave
-scp build/x86_64/linker.lds wxy:~/workspace/sgx-driver/enclave/linker.lds
-scp build/aarch64/linker.lds tmac-arm:~/workspace/sgx-driver/enclave/linker.lds
+src="x86-tmac"
+dst="arm-tmac"
+scp ${working_path}/final_main_x86_64 ${src}:~/workspace/sgx-driver/enclave/enclave
+scp ${working_path}/final_main_x86_64 ${src}:~/workspace/sgx-driver/enclave/enclave_x86_64
+scp ${working_path}/final_main_aarch64 ${src}:~/workspace/sgx-driver/enclave/enclave_aarch64
+scp ${working_path}/final_main_aarch64 ${dst}:~/workspace/sgx-driver/enclave/enclave
+scp build/x86_64/linker.lds ${src}:~/workspace/sgx-driver/enclave/linker.lds
+scp build/aarch64/linker.lds ${dst}:~/workspace/sgx-driver/enclave/linker.lds
 # scp ${working_path}/final_main_x86_64 tmac-arm:~/workspace/sgx-driver/enclave/enclave_x86_64
 # scp ${working_path}/final_main_aarch64 tmac-arm:~/workspace/sgx-driver/enclave/enclave_aarch64
--- libs/ocall_syscall_wrapper.c
+++ libs/ocall_syscall_wrapper.c
@@ -29,6 +29,9 @@
 #include "vars.h"
 
 #define MMAP_RECLAIM
+#define MMAP_DEBUG
+
+extern unsigned long enclave_start;
 
 unsigned long __brk = 0 ; //used in migration thread
 unsigned long __init_brk = 0; //used in migration thread
@@ -41,6 +44,12 @@ unsigned long __cur_mmap = 0;
 #ifdef MMAP_RECLAIM
 unsigned long __pending_free_addr = 0;
 unsigned long __pending_free_size = 0;
+#endif
+
+#ifdef MMAP_DEBUG
+# define yfzm_printf(...) printf(__VA_ARGS__)
+#else
+# define yfzm_printf(...)
 #endif
 
 //simple spin lock
@@ -208,38 +217,46 @@ long ocall_syscall2(long n, long a1, long a2)
 
     if (n == SYS_munmap)
     {
-        //printf("[SYS_munmap] addr %p, len 0x%lx\n", a1, a2);
-        if (a1 >= (unsigned long)(&heap_start) + MALLOC_AREA_SIZE &&
-                a1 < (unsigned long)(&heap_start) + MALLOC_AREA_SIZE + MMAP_AREA_SIZE) {
+        yfzm_printf("[SYS_munmap] addr %p, len 0x%lx\n", a1, a2);
+        if ((a1 >= (unsigned long)&enclave_start) && (a1 < (unsigned long)&enclave_start + 0x40000000)) {
+
+            if ((a1 >= (unsigned long)(&heap_start) + MALLOC_AREA_SIZE) &&
+                    (a1 < (unsigned long)(&heap_start) + MALLOC_AREA_SIZE + MMAP_AREA_SIZE)) {
 #ifdef MMAP_RECLAIM
-            if (a1 + a2 == __cur_mmap) {
-                if (__pending_free_addr + __pending_free_size == a1) {
-                    __pending_free_size = 0;
-                    __cur_mmap = __pending_free_addr;
-                    //printf("  [munmap] case 1: reclaim pending\n");
+                if (a1 + a2 == __cur_mmap) {
+                    if (__pending_free_addr + __pending_free_size == a1) {
+                        __pending_free_size = 0;
+                        __cur_mmap = __pending_free_addr;
+                        yfzm_printf("  [munmap] case 1: reclaim pending\n");
+                    } else {
+                        __cur_mmap = a1;
+                        yfzm_printf("  [munmap] case 2: reclaim normal\n");
+                    }
                 } else {
-                    __cur_mmap = a1;
-                    //printf("  [munmap] case 2: reclaim normal\n");
+                    if (__pending_free_addr + __pending_free_size == a1) {
+                        __pending_free_size += a2;
+                        yfzm_printf("  [munmap] case 3: merge free below\n");
+                    } else if (__pending_free_addr == a1 + a2) {
+                        __pending_free_addr = a1;
+                        __pending_free_size += a2;
+                        yfzm_printf("  [munmap] case 4: merge free above\n");
+                    } else {
+                        __pending_free_addr = a1;
+                        __pending_free_size = a2;
+                        yfzm_printf("  [munmap] case 5: record new\n");
+                    }
                 }
-            } else {
-                if (__pending_free_addr + __pending_free_size == a1) {
-                    __pending_free_size += a2;
-                    //printf("  [munmap] case 3: merge free below\n");
-                } else if (__pending_free_addr == a1 + a2) {
-                    __pending_free_addr = a1;
-                    __pending_free_size += a2;
-                    //printf("  [munmap] case 4: merge free above\n");
-                } else {
-                    __pending_free_addr = a1;
-                    __pending_free_size = a2;
-                    //printf("  [munmap] case 5: record new\n");
-                }
-            }
 #else
-            //printf("[SYS_munmap] skip enclave mmap area\n");
+                yfzm_printf("[SYS_munmap] skip enclave mmap area\n");
 #endif
-            return 0;
+                return 0;
+            }
+            printf("  [munmap] Error: unmap no-mmap area in enclave\n");
+            exit(1);
+        } else {
+            yfzm_printf("  [munmap] skip outside mmap area\n");
         }
+
     }
 
 	ptr = (unsigned long*)outside_buffer;
@@ -789,8 +806,8 @@ long ocall_syscall6(long n, long a1, long a2, long a3, long a4, long a5, long a6
 
     if (n == SYS_mmap)
     {
-        printf("[SYS_mmap] addr %p, len 0x%lx, prot %d, flags %d, fd %d, offset %lu\n", a1, a2, a3, a4, a5, a6);
-        //printf("__cur_mmap: %p\n", __cur_mmap);
+        yfzm_printf("[SYS_mmap] addr %p, len 0x%lx, prot %d, flags %d, fd %d, offset %lu\n", a1, a2, a3, a4, a5, a6);
+        yfzm_printf("__cur_mmap: %p\n", __cur_mmap);
         if (a1 == 0 && a5 == -1) {
             if (__cur_mmap == 0) {
                 __cur_mmap = (unsigned long)(&heap_start) + MALLOC_AREA_SIZE;
@@ -799,6 +816,7 @@ long ocall_syscall6(long n, long a1, long a2, long a3, long a4, long a5, long a6
 #ifdef MMAP_RECLAIM
                 if (a2 <= __pending_free_size) {
                     __pending_free_addr += a2;
+                    __pending_free_size -= a2;
                     return __pending_free_addr - a2;
                 }
 #endif
