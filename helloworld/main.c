4bedae3be3e35c13d03128e8fc77063d8bb5f025
Hao Li <yfzm1223@sjtu.edu.cn>
2020-07-28 14:31:20 +0800

    update helloworld to send more data

--- helloworld/main.c
+++ helloworld/main.c
@@ -20,6 +20,11 @@ char *x86_64_fn  = "./enclave/enclave_x86_64";
 
 int main(int argc, char* argv[])
 {
+	if (argc > 1) {
+		unsigned long size = atol(argv[1]) - 16;
+		if (size > 0)
+			printf("malloc %luMB memory at %p\n", size, malloc(size * 0x100000));
+	}
 #if 0
 	int msg_cnt;
 	msg_cnt = 0;
@@ -33,7 +38,7 @@ int main(int argc, char* argv[])
 	printf("This is the No.%d message from the app\n", ++msg_cnt);
     while (1) {
         check_migrate(1, 0);
-		// if (msg_cnt == 4) migrate(0, 0, 0);
+		if (msg_cnt == 10) return 0; 
 		printf("This is the No.%d message from the app\n", ++msg_cnt);
 		sleep(1);
     }
