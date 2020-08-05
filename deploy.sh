4bc9b6c0f8c82ada61533edc9e7b0deac7c061de
Hao Li <yfzm1223@sjtu.edu.cn>
2020-08-05 16:32:55 +0800

    transfer to target machine with distinguished names

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
-working_path=${1:-libquantum}
-(make ${working_path}) || exit 1
+obj=${1:-libquantum}
+(make ${obj}) || exit 1
 src="x86-tmac"
 dst="arm-tmac"
-scp ${working_path}/final_main_x86_64 ${src}:~/workspace/sgx-driver/enclave/enclave
-scp ${working_path}/final_main_x86_64 ${src}:~/workspace/sgx-driver/enclave/enclave_x86_64
-scp ${working_path}/final_main_aarch64 ${src}:~/workspace/sgx-driver/enclave/enclave_aarch64
-scp ${working_path}/final_main_aarch64 ${dst}:~/workspace/sgx-driver/enclave/enclave
+scp ${obj}/final_main_x86_64 ${src}:~/workspace/sgx-driver/enclave/${obj}
+scp ${obj}/final_main_x86_64 ${src}:~/workspace/sgx-driver/enclave/${obj}_x86_64
+scp ${obj}/final_main_aarch64 ${src}:~/workspace/sgx-driver/enclave/${obj}_aarch64
+scp ${obj}/final_main_aarch64 ${dst}:~/workspace/sgx-driver/enclave/${obj}
 scp build/x86_64/linker.lds ${src}:~/workspace/sgx-driver/enclave/linker.lds
 scp build/aarch64/linker.lds ${dst}:~/workspace/sgx-driver/enclave/linker.lds
-# scp ${working_path}/final_main_x86_64 tmac-arm:~/workspace/sgx-driver/enclave/enclave_x86_64
-# scp ${working_path}/final_main_aarch64 tmac-arm:~/workspace/sgx-driver/enclave/enclave_aarch64
+# scp ${obj}/final_main_x86_64 tmac-arm:~/workspace/sgx-driver/enclave/${obj}_x86_64
+# scp ${obj}/final_main_aarch64 tmac-arm:~/workspace/sgx-driver/enclave/${obj}_aarch64
